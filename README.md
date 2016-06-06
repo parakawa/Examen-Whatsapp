eb05430049d9121f728c31c3cd38cada9b4cb62c
John Doe <paty.arakawa@gmail.com>
2016-06-06 11:27:40 -0500

    ya no uso ajax, pinta data

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Whatsapp
 ===============
 
-###Para probar el primer commit abrir en Mozilla Firefox ya que el programa no tiene mantenimiento en protocolos Chrome.
+Para probar el primer commit abrir en Mozilla Firefox ya que el programa no tiene mantenimiento en protocolos Chrome.
+
+Tercer commit: Ya no uso Ajax, se puede abrir en Chrome sin problemas.
--- js/main.js
+++ js/main.js
@@ -4,32 +4,15 @@ $(document).ready(function(){
 });
 
 
-$("#search").keyup(function(){
-	var texto=$("#search").val()
-	$.getJSON("data.json",function(data){
-		var output="";
-		$.each(data,function(key,val){
-			if(val.nombre.search(texto) != -1 && texto!=""){
-			output+="<div class='panel-list-message-contact'><div class='profile-img'><img src='image/"+val.imagen+"'/></div><div class='message-text show-dots'><div class='message-text-detail'><div class='name-contact show-dots'>"+val.nombre+"</div><div class='message-hour '>"+val.mensajes[0].fecha+"</div></div><div class='message-chat show-dots'><p class='show-dots'>"+val.mensajes[0].contenido+"</p></div></div></div>"
-			
-			}
-		})
-		$(".panel-contact-chat").html(output)
-		
-	});
-
-});
 
 function llenado(){
-	$.getJSON("data.json",function(data){
-		var output="";
-		$.each(data,function(key,val){
-			output+="<div class='panel-list-message-contact'><div class='profile-img'><img src='image/"+val.imagen+"'/></div><div class='message-text show-dots'><div class='message-text-detail'><div class='name-contact show-dots'>"+val.nombre+"</div><div class='message-hour '>"+val.mensajes[0].fecha+"</div></div><div class='message-chat show-dots'><p class='show-dots'>"+val.mensajes[0].contenido+"</p></div></div></div>"
-
-		})
-		$(".panel-contact-chat").html(output)
-		
-	});
+	tam=datos.length
+	for(var i=0;i<tam;i++){
+		var div=document.createElement("div")
+		div.setAttribute("class","panel-list-message-contact")
+		div.innerHTML="<div class='profile-img'><img src='image/"+datos[i].imagen+"'/></div><div class='message-text show-dots'><div class='message-text-detail'><div class='name-contact show-dots'>"+datos[i].nombre+"</div><div class='message-hour '>"+datos[i].mensajes[0].fecha+"</div></div><div class='message-chat show-dots'><p class='show-dots'>"+datos[i].mensajes[0].contenido+"</p></div></div>"
+		$(".panel-contact-chat").append(div)
+	}
 }
 
 
@@ -56,7 +39,7 @@ function llenado(){
 			else {
 				output="<p>No se encontró</p>"
 				$(".panel-contact-chat").html(output)
-			}
+			}○
 
 		});	
 	});
